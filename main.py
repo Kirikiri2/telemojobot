eb1f1374111ee6b45bd1718f3bfec438c5a3171c
Андрей <andrew777chzen@gmail.com>
2024-04-26 14:47:57 +0300

    проверка

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import telebot
-
-bot = telebot.TeleBot('6315152427:AAFDco_qQgHRHkw5bxXfXfAPmoU5sHIaeCU')
+from telebot import types
+bot = telebot.TeleBot('6315152427:AAFDco_qQgHRHkw5bxXfXfAPmoU5sHIaeCU') #Нашен токен
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, f"Hello, {message.from_user.first_name} {message.from_user.last_name}")
+    bot.send_message(message.chat.id, f"✌️Peace, {message.from_user.first_name} {message.from_user.last_name}")
 
 bot.infinity_polling()
-
-
-
-
