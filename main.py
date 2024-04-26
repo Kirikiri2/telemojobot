828fb012bb5a2f409efa7a3b98fa95aa9845530b
Kirikiri2 <worklisa271@gmail.com>
2024-04-26 14:01:39 +0300

    начало

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+import telebot
+
+bot = telebot.TeleBot('6315152427:AAFDco_qQgHRHkw5bxXfXfAPmoU5sHIaeCU')
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, f"Hello, {message.from_user.first_name} {message.from_user.last_name}")
+
+bot.infinity_polling()
