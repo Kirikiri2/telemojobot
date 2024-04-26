c476e2089f27f4ff4c5a3b50f499b0f464910cc9
Андрей <andrew777chzen@gmail.com>
2024-04-26 15:29:58 +0300

    проверка

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ bot = telebot.TeleBot('6315152427:AAFDco_qQgHRHkw5bxXfXfAPmoU5sHIaeCU') #Наш�
 def main(message):
 
    # bot.send_message(message.chat.id, f"✌️Peace, {message.from_user.first_name} {message.from_user.last_name}")
-    first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nХочешь проверить статус заказа?"
+    first_mess = f"<b>{message.from_user.first_name}</b>, привет!\nХочешь проверить статус заказа?"
     markup = types.InlineKeyboardMarkup()
     button_yes = types.InlineKeyboardButton(text = 'Да', callback_data='yes')
     markup.add(button_yes)
     bot.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
+@bot.callback_query_handler(func=lambda call:True)
+def response(function_call):
+  if function_call.message:
+     if function_call.data == "yes":
+        agreement = "Введите номер заказа"
+        markup = types.InlineKeyboardMarkup()
+        bot.send_message(function_call.message.chat.id, agreement, reply_markup=markup)
+        bot.answer_callback_query(function_call.id)
 bot.infinity_polling()
