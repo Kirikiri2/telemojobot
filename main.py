c7fdafd1eee86c7fcec8d54b76ca3b5a250a07fa
Kirikiri2 <worklisa271@gmail.com>
2024-04-27 18:50:37 +0300

    окончательно сделала проверку оплачено/не оплачено для зарег qr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+import requests
 
 bot = telebot.TeleBot('6315152427:AAFDco_qQgHRHkw5bxXfXfAPmoU5sHIaeCU') #Нашен токен
 
@@ -10,9 +11,11 @@ def main(message):
    # bot.send_message(message.chat.id, f"✌️Peace, {message.from_user.first_name} {message.from_user.last_name}")
     first_mess = f"<b>{message.from_user.first_name}</b>, привет!\nХочешь проверить статус заказа?"
     markup = types.InlineKeyboardMarkup()
-    button_yes = types.InlineKeyboardButton(text = 'Да', callback_data='yes')
+    button_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
     markup.add(button_yes)
     bot.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
+
+
 @bot.callback_query_handler(func=lambda call:True)
 def response(function_call):
   if function_call.message:
@@ -21,4 +24,50 @@ def response(function_call):
         markup = types.InlineKeyboardMarkup()
         bot.send_message(function_call.message.chat.id, agreement, reply_markup=markup)
         bot.answer_callback_query(function_call.id)
+@bot.message_handler()
+def id(message):
+    if message.text.lower() == '001.111.111':
+        url = "https://pay-test.raif.ru/api/sbp/v1/qr/AD3D919E91494C53A41C5EFEB837846B/payment-info"
+
+        # Заголовок с токеном авторизации
+        headers = {
+            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU"
+        }
+
+        # Отправка GET-запроса
+        response = requests.get(url, headers=headers)
+
+        # Проверка статуса ответа
+        if response.status_code == 200:
+            response_data = response.json()
+            status = response_data['paymentStatus']
+            if status == "SUCCESS":
+                bot.send_message(message.chat.id,"оплачено")
+            else:
+                bot.send_message(message.chat.id,"не оплачено")
+        else:
+           bot.send_message(message.chat.id,"Ошибка при выполнении запроса:", response.status_code)
+    if message.text.lower() == '123.578.000':
+        url = "https://pay-test.raif.ru/api/sbp/v1/qr/AD7AE6A28D04438FAA9727E2644EB037/payment-info"
+
+        headers = {
+            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU"
+        }
+
+        # Отправка GET-запроса
+        response = requests.get(url, headers=headers)
+
+        # Проверка статуса ответа
+        if response.status_code == 200:
+            response_data = response.json()
+            status = response_data['paymentStatus']
+            if status == "SUCCESS":
+                bot.send_message(message.chat.id, "оплачено")
+            else:
+                bot.send_message(message.chat.id, "не оплачено")
+        else:
+            bot.send_message(message.chat.id, "Ошибка при выполнении запроса:", response.status_code)
+
+
+
 bot.infinity_polling()
