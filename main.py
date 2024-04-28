f04182cfb8c8b09d08b7207a144cedb478f50b53
Kirikiri2 <worklisa271@gmail.com>
2024-04-28 08:43:03 +0300

    окончательно сделала проверку оплачено/не оплачено для зарег qr утренняя версия

--- main.py
+++ main.py
@@ -26,52 +26,92 @@ def response(function_call):
         bot.answer_callback_query(function_call.id)
 @bot.message_handler()
 def id(message):
-    if message.text.lower() == '/mojo 001.111.111' or message.text.lower() == '/mojo 005.111.111':
-        url = "https://pay-test.raif.ru/api/sbp/v1/qr/AD3D919E91494C53A41C5EFEB837846B/payment-info"
+    if message.text.lower() == '/mojo 001.111.111' or message.text.lower() == '/mojo 555.111.111':
+        if message.text.lower() == '/mojo 001.111.111':
+            url = "https://pay-test.raif.ru/api/sbp/v1/qr/AD3D919E91494C53A41C5EFEB837846B/payment-info"
 
-        # Заголовок с токеном авторизации
-        headers = {
-            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU"
-        }
+            # Заголовок с токеном авторизации
+            headers = {
+                "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU"
+            }
 
-        # Отправка GET-запроса
-        response = requests.get(url, headers=headers)
+            # Отправка GET-запроса
+            response = requests.get(url, headers=headers)
 
-        # Проверка статуса ответа
-        if response.status_code == 200:
-            response_data = response.json()
-            status = response_data['paymentStatus']
-            if status == "SUCCESS":
-                bot.send_message(message.chat.id,"Заказ оплачен.")
+            # Проверка статуса ответа
+            if response.status_code == 200:
+                response_data = response.json()
+                status = response_data['paymentStatus']
+                if status == "SUCCESS":
+                    bot.send_message(message.chat.id, "Заказ оплачен.")
+                else:
+                    bot.send_message(message.chat.id, "Заказ не оплачен.\nПовторите запрос для обновления статуса")
             else:
-                bot.send_message(message.chat.id,"Заказ не оплачен.\nПовторите запрос для обновления статуса")
-        else:
-           bot.send_message(message.chat.id,"Ошибка при выполнении запроса:", response.status_code)
+                bot.send_message(message.chat.id, "Ошибка при выполнении запроса:", response.status_code)
+        if message.text.lower() == '/mojo 555.111.111':
+            url = "https://pay-test.raif.ru/api/sbp/v1/qr/AD04CDC45D4B4E3EB24CD2F1FD6F84FF/payment-info"
+
+            # Заголовок с токеном авторизации
+            headers = {
+                "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU"
+            }
+
+            # Отправка GET-запроса
+            response = requests.get(url, headers=headers)
+
+            # Проверка статуса ответа
+            if response.status_code == 200:
+                response_data = response.json()
+                status = response_data['paymentStatus']
+                if status == "SUCCESS":
+                    bot.send_message(message.chat.id, "Заказ оплачен.")
+                else:
+                    bot.send_message(message.chat.id, "Заказ не оплачен.\nПовторите запрос для обновления статуса")
+            else:
+                bot.send_message(message.chat.id, "Ошибка при выполнении запроса:", response.status_code)
+
     elif message.text.lower() == '/mojo 044.111.111' or message.text.lower() == '/mojo 009.111.111':
-        url = "https://pay-test.raif.ru/api/sbp/v1/qr/AD48EF32CC3049818875BF813A270CB1/payment-info"
+        if message.text.lower() == '/mojo 044.111.111':
+            url = "https://pay-test.raif.ru/api/sbp/v1/qr/AD02D3A781054D52BFE4654CCA8CCF1D/payment-info"
 
-        headers = {
-            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU"
-        }
+            headers = {
+                "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU"
+            }
 
-        # Отправка GET-запроса
-        response = requests.get(url, headers=headers)
+            # Отправка GET-запроса
+            response = requests.get(url, headers=headers)
 
-        # Проверка статуса ответа
-        if response.status_code == 200:
-            response_data = response.json()
-            status = response_data['paymentStatus']
-            if status == "SUCCESS":
-                bot.send_message(message.chat.id, "Заказ оплачен.")
+            # Проверка статуса ответа
+            if response.status_code == 200:
+                response_data = response.json()
+                status = response_data['paymentStatus']
+                if status == "SUCCESS":
+                    bot.send_message(message.chat.id, "Заказ оплачен.")
+                else:
+                    bot.send_message(message.chat.id, "Заказ не оплачен.\nПовторите запрос для обновления статуса")
             else:
-                bot.send_message(message.chat.id, "Заказ не оплачен.\nПовторите запрос для обновления статуса")
-                first_mess = f"</b>Желаете ли вы повторить запрос?"
-                markup = types.InlineKeyboardMarkup()
-                button_yes = types.InlineKeyboardButton(text='Обновить', callback_data='yes')
-                markup.add(button_yes)
-                bot.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
-        else:
-            bot.send_message(message.chat.id, "Ошибка при выполнении запроса:", response.status_code)
+                bot.send_message(message.chat.id, "Ошибка при выполнении запроса:", response.status_code)
+        if message.text.lower() == '/mojo 009.111.111':
+            url = "https://pay-test.raif.ru/api/sbp/v1/qr/AD4E6B5A010E4C42ADABB0FE142D092D/payment-info"
+
+            headers = {
+                "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU"
+            }
+
+            # Отправка GET-запроса
+            response = requests.get(url, headers=headers)
+
+            # Проверка статуса ответа
+            if response.status_code == 200:
+                response_data = response.json()
+                status = response_data['paymentStatus']
+                if status == "SUCCESS":
+                    bot.send_message(message.chat.id, "Заказ оплачен.")
+                else:
+                    bot.send_message(message.chat.id, "Заказ не оплачен.\nПовторите запрос для обновления статуса")
+            else:
+                bot.send_message(message.chat.id, "Ошибка при выполнении запроса:", response.status_code)
+
     else:
         bot.send_message(message.chat.id, "Неверный формат ID или незарегистированный ID.")
 
